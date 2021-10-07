d4df6ea64bcc48fb52579f83b48471d9758f9024
Yuliia <ylua.vasylyk.04@gmail.com>
2021-10-07 10:27:23 +0300

    hello

--- lab 3.4/lab 3.4/Source.cpp
+++ lab 3.4/lab 3.4/Source.cpp
@@ -9,15 +9,18 @@ using namespace std;
 int main()
 {
     double x; // вхідний аргумент
-    double R{}; // вхідний параметр
+    double R; // вхідний параметр
     double y; // вхідний параметр
     cout << "x = "; cin >> x;
     cout << "y = "; cin >> y;
+    cout << "R = "; cin >> R;
 
-    if ((1. / 4) * (x * x) + (1. / 4) * (y * y) <= ((1. / 4) * (R * R)) || y >= -2 * x && y <= -1 / 2 * x)
+    if ((x >= -R && x <= 0 && y <= R && y >= 0 && y * y <= R * R - x * x) || (x > 0 && x <= R / 2 && y >= R && y <= 0 && y >= 2 * x) || (x >= R / 2 && x <= R && y >= R && y <= 0 && y >= 2 * x - 2 * R))
 
         cout << "yes" << endl;
     else
         cout << "no" << endl;
     return 0;
 }
+
+
